bdcdd1807645ff969a7f9e892bad531f1e35f48c
Carlos Suarez <carlos.srz@protonmail.com>
2025-04-24 08:33:20 +0100

    Initial Commit

---
+++ PointPro/PointProApp.swift
@@ -0,0 +1,32 @@
+//
+//  PointProApp.swift
+//  PointPro
+//
+//  Created by Carlos Suarez on 24/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PointProApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
